c994965c46bd3a7c31a770346bc9b2a49eccf30e
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-09-24 11:07:44 +0100

    ft--api connected

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,17 +4,44 @@ import Image from 'next/image'
 import ManImg from '../assets/3d.png'
 import WalkmanImg from '../assets/walkm.png'
 import WalkwomanImg from '../assets/walkw.png'
+import axios from 'axios'
 
 const register = () => {
   const [team_name, setTeam_name] = useState('')
   const [phone_number, setPhone_number] = useState('')
   const [email, setEmail] = useState('')
   const [project_topic, setProject_topic] = useState('')
+  const [category, setCategory] = useState(0)
   const [group_size, setGroup_size] = useState(0)
   const [privacy_policy, setPrivacy_policy] = useState(false)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
+    try {
+      await axios
+        .post(`https://backend.getlinked.ai/hackathon/registration`, {
+          team_name,
+          phone_number,
+          email,
+          project_topic,
+          category,
+          group_size,
+          privacy_policy,
+        })
+        .then((response) => {
+          console.log(response)
+          {/**Alert */}
+          setTeam_name('')
+          setCategory(0)
+          setEmail('')
+          setPhone_number('')
+          setGroup_size(0)
+          setPrivacy_policy(false)
+          setProject_topic('')
+        })
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <section className='flex w-full my-4'>
@@ -41,7 +68,9 @@ const register = () => {
               <div className='md:w-[95%]'>
                 <label>Team’s Name</label>
                 <input
+                  required
                   value={team_name}
+                  onChange={(e) => setTeam_name(e.target.value)}
                   placeholder='Enter the name of your group'
                   className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'
                 />
@@ -49,7 +78,9 @@ const register = () => {
               <div>
                 <label>Phone</label>
                 <input
+                  required
                   value={phone_number}
+                  onChange={(e) => setPhone_number(e.target.value)}
                   placeholder='Enter your phone number'
                   className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'
                 />
@@ -59,7 +90,9 @@ const register = () => {
               <div className='md:w-[95%]'>
                 <label>Email</label>
                 <input
+                  required
                   value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   placeholder='Enter your email address'
                   className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'
                 />
@@ -67,7 +100,9 @@ const register = () => {
               <div>
                 <label>Project Topic</label>
                 <input
+                  required
                   value={project_topic}
+                  onChange={(e) => setProject_topic(e.target.value)}
                   placeholder='What is your group project topic'
                   className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'
                 />
@@ -76,16 +111,23 @@ const register = () => {
             <div className='grid md:grid-cols-2 gap-1 my-5'>
               <div className='md:w-[95%]'>
                 <label>Category</label>
-                <select className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'>
+                <select
+                  required
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'>
                   <option>Select your category</option>
-                  <option>Mobile</option>
-                  <option>Web</option>
-                  <option>Backend</option>
+                  <option value='1'>Mobile</option>
+                  <option value='2'>Web</option>
+                  <option value='3'>Backend</option>
                 </select>
               </div>
               <div>
                 <label htmlFor='group-size'>Group Size</label>
                 <select
+                  required
+                  value={group_size}
+                  onChange={(e) => setGroup_size(e.target.value)}
                   id='group-size'
                   name='group-size'
                   className='border border-white bg-transparent w-full rounded px-2 py-1 outline-none'>
@@ -103,9 +145,13 @@ const register = () => {
               <p className='text-[#D434FE]'>
                 Please review your registration details before submitting
               </p>
-              <div className='flex items-center justify-between'>
-                <input type='checkbox' />
-                <p>
+              <div className='flex w-full items-center justify-between'>
+                <input
+                  type='checkbox'
+                  value={privacy_policy}
+                  onChange={(e) => setPrivacy_policy(true)}
+                />
+                <p className='text-sm'>
                   I agreed with the event terms and conditions and privacy
                   policy
                 </p>
